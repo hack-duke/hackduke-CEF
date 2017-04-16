8bee07e24afa068235c56457ce6a48d464d846f7
Geng <snggeng@gmail.com>
2017-04-16 16:35:36 -0400

    successfully loaded data onto demo localhost via handsontable.js

--- website/index.js
+++ website/index.js
@@ -29,8 +29,18 @@ var $$ = function(id) {
   hot = new Handsontable(container, {
     startRows: 700,
     startCols: 6,
+    dataSchema: {address: null, classification: null, dateListed: null, pricePerMonth: null, specs: null},
     rowHeaders: true,
-    colHeaders: true,
+    colHeaders: ['address', 'classification', 'dateListed', 'pricePerMonth', 'specs'],
+    columns: [
+      {data: 'address'},
+      {data: 'classification'},
+      {data: 'dateListed'},
+      {data: 'pricePerMonth'},
+      {data: 'specs'}
+    ],
+    manualColumnResize: true,
+    manualRowResize: true,
     afterChange: function (change, source) {
       if (source === 'loadData') {
         return; //don't save this change
@@ -39,37 +49,63 @@ var $$ = function(id) {
         return;
       }
       clearTimeout(autosaveNotification);
-      // $.ajax('scripts/json/save.json', 'GET', JSON.stringify({data: change}), function (data) {
-      //   exampleConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')';
-      //   autosaveNotification = setTimeout(function() {
-      //     exampleConsole.innerText ='Changes will be autosaved';
-      //   }, 1000);
-      // });
+      $.ajax('json/save.json', 'GET', JSON.stringify({data: change}), function (data) {
+        exampleConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')';
+        autosaveNotification = setTimeout(function() {
+          exampleConsole.innerText ='Changes will be autosaved';
+        }, 1000);
+      });
     }
   });
 
   Handsontable.Dom.addEvent(load, 'click', function() {
-    $.ajax('json/load.json', 'GET', '', function(res) {
-      var data = JSON.parse(res.response);
-
-      hot.loadData(data.data);
-      exampleConsole.innerText = 'Data loaded';
-    });
+    console.log("clicked load")
+    // $.ajax('json/load.json', 'GET', '', function(res, err) {
+    //   console.log("ajax start")
+    //   var data = JSON.parse(res.response);
+    //   console.log("1", data)
+    //   console.log("2", data.data)
+    //   hot.loadData(data);
+    //   exampleConsole.innerText = 'Data loaded';
+    // });
+    $.ajax({
+          type: 'GET',
+           url: 'json/load.json',
+           async: false,
+           contentType: "application/json",
+           dataType: 'json',
+           success: function(res) {
+             console.log(res);
+             var data = res;
+             console.log(data);
+             hot.loadData(data);
+             exampleConsole.innerText = 'Data loaded';
+          },
+           error: function(e) {
+              console.log(e.message);
+              // console.log(JSON.parse(e.responseText)[0]['address'])
+              // var data = JSON.parse(e.responseText)[0]
+              //console.log(data)
+              // hot.loadData(data);
+              // exampleConsole.innerText = 'Data loaded';
+           }
+          })
   });
 
-  // Handsontable.Dom.addEvent(save, 'click', function() {
-  //   // save all cell's data
-  //   ajax('scripts/json/save.json', 'GET', JSON.stringify({data: hot.getData()}), function (res) {
-  //     var response = JSON.parse(res.response);
-  //
-  //     if (response.result === 'ok') {
-  //       exampleConsole.innerText = 'Data saved';
-  //     }
-  //     else {
-  //       exampleConsole.innerText = 'Save error';
-  //     }
-  //   });
-  // });
+  Handsontable.Dom.addEvent(save, 'click', function() {
+    // save all cell's data
+    console.log("clicked save")
+    $.ajax('json/save.json', 'GET', JSON.stringify({data: hot.getData()}), function (res) {
+      var response = JSON.parse(res.response);
+
+      if (response.result === 'ok') {
+        exampleConsole.innerText = 'Data saved';
+      }
+      else {
+        exampleConsole.innerText = 'Save error';
+      }
+    });
+  });
 
   Handsontable.Dom.addEvent(autosave, 'click', function() {
     if (autosave.checked) {
