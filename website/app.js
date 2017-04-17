2c32f61b776f0d46d1e09adaf2c8302f4eba060b
Geng <snggeng@gmail.com>
2017-04-16 23:24:05 -0400

    app.use static

--- website/app.js
+++ website/app.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 
+// serve static files
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
